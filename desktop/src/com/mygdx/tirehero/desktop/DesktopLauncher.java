8845169059d5928a2458ec18d23fdfa0270e959e
Vladimir Karmishin <vlat456@gmail.com>
2015-04-06 05:38:12 +0200

    Initial

---
+++ desktop/src/com/mygdx/tirehero/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.tirehero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.tirehero.TireHero;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TireHero(), config);
+	}
+}
